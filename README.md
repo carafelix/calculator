692473bd549455800dc5c441914b75183fc44180
carafelix <heroprotagonist32@gmail.com>
2023-07-19 23:06:01 -0400

    html skeleton

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ calculator calculating arithmetics, no so fun I must say
 ## todo
 - = if num is empty, repeat last operation on the current number
 - display num op num =
-
+- display overflow horizontal scroll
+- if a == str && op == '=' && b == number, add str to a global variables object
+- display obj
--- src/js/calculator.ts
+++ src/js/calculator.ts
@@ -1,3 +1,5 @@
+// ---------- operatorial functions
+
 function sum (a:number,b:number):number{
     return a+b
 }
@@ -10,12 +12,53 @@ function divide (a:number,b:number):number{
 function multiply (a:number,b:number):number{
     return a*b
 }
+function factor (a:number,b:number):number{
+    return a**b
+}
 
+function modulo (a:number,b:number):number{
+    return a%b
+}
+
+// ---------- operatorial variables
 
 let operandA:number;
 let operator:string;
 let operandB:number;
 
-function operate(){
+
+function operate(a:number | string, op:string,  b:number) :number | void {
     
+    
+    if (op === '+' && typeof a === 'number' ){
+
+        return sum(a,b);
+
+    } else if(op === '-' && typeof a === 'number'){
+
+        return substract(a,b);
+
+    } else if (op === '/' && typeof a === 'number'){
+
+        return divide(a,b);
+
+    } else if (op === '*' && typeof a === 'number'){
+
+        return multiply(a,b);
+
+    } else if (op === '^' && typeof a === 'number'){
+
+        return factor(a,b);
+
+    } else if (op === '%' && typeof a === 'number'){
+
+        return modulo(a,b);
+
+    } else if (op === '=' && typeof a === 'string' && typeof b === 'number'){
+
+        return //make gobal object variable
+
+    } else {
+        throw new Error('what are you doing?');
+    }
 }
