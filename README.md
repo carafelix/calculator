40deeafd9da99166431230d3f75530f4527e89ed
carafelix <heroprotagonist32@gmail.com>
2023-07-19 21:19:35 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # calculator
 calculator calculating arithmetics, no so fun I must say
+
+## todo
+- = if num is empty, repeat last operation on the current number
+- display num op num =
+
---
+++ src/js/calculator.ts
@@ -0,0 +1,21 @@
+function sum (a:number,b:number):number{
+    return a+b
+}
+function substract (a:number,b:number):number{
+    return a-b
+}
+function divide (a:number,b:number):number{
+    return a/b
+}
+function multiply (a:number,b:number):number{
+    return a*b
+}
+
+
+let operandA:number;
+let operator:string;
+let operandB:number;
+
+function operate(){
+    
+}
