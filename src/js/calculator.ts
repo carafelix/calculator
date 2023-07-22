080697bd45248c1a3493564acf273f00a4fcd1a4
carafelix <heroprotagonist32@gmail.com>
2023-07-22 10:43:51 -0400

    initial preOperate logic

--- src/js/calculator.ts
+++ src/js/calculator.ts
@@ -3,6 +3,8 @@
 const numbers = document.querySelectorAll('.number button');
 const operators = document.querySelectorAll('.operator button');
 const commands = document.querySelectorAll('.command button');
+const log = document.querySelector('#log');
+const result = document.querySelector('#result');
 
 // ---------- operatorial functions
 
@@ -29,7 +31,7 @@ function modulo (a:number,b:number):number{
 // ---------- operatorial variables
 
 let operandA:number;        let preOperandA:string = '';
-let operator:string;
+let operator:string = '';
 let operandB:number;        let preOperandB:string = '';
 
 
@@ -70,17 +72,45 @@ function operate(a:number | string, op:string,  b:number) :number | void {
 }
 
 // ----------- listener functions 
+//document.addEventListener('keydown', (e)=>asignOperand(e))
 
 numbers.forEach((n) => n.addEventListener('click', (e)=>asignOperand(e) ));
-document.addEventListener('keydown', (e)=>asignOperand(e))
+operators.forEach((n) => n.addEventListener('click', (e)=>asignOperator(e) ));
 
 
 // add number to operand, if A as a value, add it to B
 
 function asignOperand(e: Event){
-    if (e instanceof MouseEvent && e.target instanceof HTMLButtonElement){
-        preOperandA += (e.target.innerText)
-    } else if (e instanceof KeyboardEvent){
-        console.log(e)
+    if (e instanceof MouseEvent &&
+        e.target instanceof HTMLButtonElement &&
+        operator === '' && preOperandB === ''){
+
+        preOperandA += e.target.innerText;
+
+    } else if (e instanceof MouseEvent &&
+        e.target instanceof HTMLButtonElement &&
+        preOperandA !== '' && operator !== ''){
+
+            preOperandB += e.target.innerText;
+
+        }
+}
+
+function asignOperator(e: Event){
+
+    if (e instanceof MouseEvent &&
+        e.target instanceof HTMLButtonElement &&
+        preOperandA !== '' && preOperandB === ''){
+            
+            if(e.target.lastChild instanceof HTMLSpanElement &&
+               e.target.lastChild.textContent){
+               operator = e.target.lastChild.textContent;
+            }
+            else{
+                operator = e.target.innerText;
+            }
     }
+
+    //update display function
+
 }
